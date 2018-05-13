d6909775c8ac902dabcffa7b5cb76b6c487d23ae
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-05-13 06:01:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function getFirstSelector(selector) {
+  const element = document.querySelector(selector);
+  return element
+}
+
+function nestedTarget() {
+ const element = document.getElementById('nested').querySelector('.target');
+ return element
+}
+
+
+function deepestChild(array, criterianFn) {
+  let current = array;
+  let next = [];
+      for (let i =0; i < current.length; i++) {
+        next.push(current[i])
+      }
+   
+  
+  
+}
+
+
+function increaseRankBy(n) {
+  const ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i <ranks.length; i++) {
+    ranks[i].innerHTML = (parseInt(ranks[i].innerHTML) + n).toString()
+  }
+}
